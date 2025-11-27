c6b9ef9e0caeb52ca6cb20411269cd1451984e96
Freddy Guaman <freddyguaman20@outlook.es>
2025-11-27 09:07:36 -0500

    actulizacion

--- db.php
+++ db.php
@@ -1,11 +1,12 @@
 <?php
-$host = "localhost";
-$puerto = 3307;
-$user = "root";
-$pass = "";
-$db = "mi_sitio";
+$host = "sql306.infinityfree.com";
+$puerto = 3306;
+$user = "if0_40452673";
+$pass = "Feri9798";
+$db = "if0_40452673_mi_sitio";
 
 $conn = new mysqli($host, $user, $pass, $db, $puerto);
+
 if ($conn->connect_error) {
   echo("Conexión fallida: " . $conn->connect_error);
 }
